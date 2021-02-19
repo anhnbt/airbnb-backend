0ffaab262eda2aefb9df96c65c45c10602c22659
Nguyenn123 <dongcoxanh1001@gmail.com>
2021-02-19 10:21:55 +0700

    update AuthController.java

--- src/main/java/com/codegym/airbnb/repositories/BookingRepository.java
+++ src/main/java/com/codegym/airbnb/repositories/BookingRepository.java
@@ -4,6 +4,9 @@ import com.codegym.airbnb.model.Booking;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Long> {
+    Optional<Booking> findByRoomIdAndUserId(Long id, Long id_user);
 }
